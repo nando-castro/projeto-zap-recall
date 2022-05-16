3d1acaa1f4a6d7508dc683f9181ab88e91c6adf5
nando-castro <fernando.castro10.master@gmail.com>
2022-05-16 12:13:35 -0300

    Correção de bug tela inicial

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -3,7 +3,7 @@ import Init from "../Init/Init";
 
 export default function Content({ entrar }) {
   function Button() {
-    return <a onClick={() => entrar("")}>Iniciar recall!</a>;
+    return <a onClick={() => entrar("cards")}>Iniciar recall!</a>;
   }
 
   return (
