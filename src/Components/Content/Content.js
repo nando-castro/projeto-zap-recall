bf08cc5ae6f379b64b4580d2ac473cd7d2b49207
nando-castro <fernando.castro10.master@gmail.com>
2022-05-16 12:36:53 -0300

    Add de input: bônus

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -6,10 +6,15 @@ export default function Content({ entrar }) {
     return <a onClick={() => entrar("cards")}>Iniciar recall!</a>;
   }
 
+  function Input() {
+    return <input className="input" type="number" placeholder="Digite sua meta de zaps..."></input>
+  }
+
   return (
     <>
       <div className="home">
         <Init classe={"logo"} />
+        <Input />
         <Button />
       </div>
     </>
